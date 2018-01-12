644b1b8a33fc247dee2a0bbac880cc5a0eee53d1
Prashant Singh <psingh@orkutics.com>
2018-01-12 01:15:07 +0000

    Eureka integration to connect with Eureka Server

--- src/main/java/co/uk/app/commerce/order/application/OrderApplication.java
+++ src/main/java/co/uk/app/commerce/order/application/OrderApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -12,6 +13,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({ "co.uk.app.commerce.order", "co.uk.app.commerce.basket", "co.uk.app.commerce.address",
 		"co.uk.app.commerce.summary", "co.uk.app.commerce.delivery", "co.uk.app.commerce.shipping",
