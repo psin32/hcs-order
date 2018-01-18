ed92b50e160cfdfc7a6bc6a13994ee33ff94a0ac
Prashant Singh <psingh@orkutics.com>
2018-01-18 00:41:22 +0000

    Disabling kafka in orders application

--- src/main/java/co/uk/app/commerce/kafka/config/OrdersKafkaStreamConfig.java
+++ src/main/java/co/uk/app/commerce/kafka/config/OrdersKafkaStreamConfig.java
@@ -18,7 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
@@ -33,7 +32,7 @@ import co.uk.app.commerce.address.repository.AddressRepository;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 
-@Configuration
+//@Configuration
 @EnableKafka
 @EnableKafkaStreams
 public class OrdersKafkaStreamConfig {
